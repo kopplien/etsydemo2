57c3af903652e101730f46b818d75123aa9b29a0
Kimberly Kopplien <k.kopplien@comcast.net>
2015-06-02 08:49:16 -0700

    test

---
+++ app/models/listing.rb
@@ -0,0 +1,7 @@
+class Listing < ActiveRecord::Base
+	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "missing.png"
+	# validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	#  Paperclip.option[:content_type_mappings] = { :png => "image"}
+	
+end
