df1564f6857cf79b496af0a793b4e28c2d5ba973
Kimberly Kopplien <k.kopplien@comcast.net>
2015-07-23 10:20:54 -0700

    add validation for user and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -15,5 +15,10 @@ class Listing < ActiveRecord::Base
 	 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 	#  Paperclip.option[:content_type_mappings] = { :png => "image"}
-	
+
+	validates :name, :description, :price, presence: true
+	# must be numeric and greater than 10  
+	validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}
+
+	# this is so images cannot be blank validates_attachment_presence :image
 end
