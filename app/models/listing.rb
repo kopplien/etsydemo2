f934dab4ba5d653db1c9f70da7f99d8aa523c3eb
Kimberly Kopplien <k.kopplien@comcast.net>
2015-07-23 13:51:23 -0700

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,23 +2,29 @@ class Listing < ActiveRecord::Base
 
 	if Rails.env.devlopment?
 		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "missing.png"
+
+		# relationship of listing and users db 
+		belongs_to :user
 	else
 
 	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "missing.png",
-			:storage => :dropbox,
-    		:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    		:path => ":stle/:id_:filename"
+		:storage => :dropbox,
+    	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    	:path => ":style/:id_:filename"
+
+    	# relationship of listing and users db 
+		belongs_to :user
     end
     		
 	# validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-	#  Paperclip.option[:content_type_mappings] = { :png => "image"}
-
 	validates :name, :description, :price, presence: true
 	# must be numeric and greater than 10  
 	validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}
 
 	# this is so images cannot be blank validates_attachment_presence :image
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
+
+  # one to many relationship with listings - listing depends upon the user who created it = user deletion removes listings
+  has_many :listings, dependent: :destroy
+
 end
